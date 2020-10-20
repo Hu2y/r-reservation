442803d33dd24e81fe41435e66ee203feef2faf9
Huey <sanghun_@daum.net>
2020-10-20 17:57:09 +0900

    Feat: 로그인 페이지 라우팅 연결

--- src/modules/define/path.ts
+++ src/modules/define/path.ts
@@ -2,6 +2,7 @@ import IPath  from '@/@types/Path.ts';
 
 export const pathUri: IPath = {
   main: '/',
+  login: '/login',
 };
 
 export const getPath = (_pathname: string) => Object.keys(pathUri).find((key) => pathUri[key] === _pathname);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import { pathUri } from '@/modules/define/path';
 
 import MainPage from '@/pages/Main.pages';
+import LoginPage from '@/pages/Login.pages';
 
 const Routes = () => (
 	<Switch>
 		<Route exact path={pathUri.main} component={MainPage} />
+		<Route exact path={pathUri.login} component={LoginPage} />
 	</Switch>
 );
 
