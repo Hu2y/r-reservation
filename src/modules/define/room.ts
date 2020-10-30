9fa858d67c2348de8d169287e4ac20ca6c336975
한상훈 <shhan@rsupport.com>
2020-10-30 18:52:33 +0900

    Feat: 회의실 데이터 생성 및 컴포넌트 생성

---
+++ src/modules/define/room.ts
@@ -0,0 +1,34 @@
+export const roomInfo = {
+  room: [
+    {
+      layer: "9층",
+      reservationRooms: [
+        "크롬", "사파리", "엣지"
+      ]
+    },
+    {
+      layer: "10층",
+      reservationRooms: [
+        "파이어폭스", "토치", "비발디"
+      ]
+    },
+    {
+      layer: "11층",
+      reservationRooms: [
+        "오페라",
+      ]
+    },
+    {
+      layer: "12층",
+      reservationRooms: [
+        "미도리", "아반트", "맥스톤"
+      ]
+    },
+    {
+      layer: "15층",
+      reservationRooms: [
+        "슬림젯", "test1", "test2"
+      ]
+    }
+  ]
+}
